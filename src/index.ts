2a904d6fb7ebe8a4c546bfc2a7262b9ca906dba7
Matthew Podwysocki <matthewp@microsoft.com>
2021-02-04 20:09:30 -0500

    Updating formatting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { NotificationHubService } from 'azure-sb';
-import { HUB_NAME, CONNECTION_STRING, GCM_REGISTRATION_ID } from './constants';
+import { HUB_NAME, CONNECTION_STRING, APNS_REGISTRATION_ID, GCM_REGISTRATION_ID } from './constants';
 
 const SAMPLE_TAG = 'language_en';
 
@@ -14,25 +14,58 @@ const GCM_MESSAGE = {
 	}
 }
 
+const APNS_MESSAGE = {
+  aps: {
+    alert: {
+      "title": "Notification Hub Test Notification",
+      "body": "This is a sample notification delivered by Azure Notification Hubs."
+    }
+  }
+};
+
 const service = new NotificationHubService(HUB_NAME, CONNECTION_STRING, '', '');
 
-service.gcm.createNativeRegistration(GCM_REGISTRATION_ID, SAMPLE_TAG, (err, response) => {
-
+if (APNS_REGISTRATION_ID) {
+  service.apns.createNativeRegistration(APNS_REGISTRATION_ID, SAMPLE_TAG, (err, response) => {
     if (err) {
-        console.log(err);
-        return;
+      console.log(err);
+      return;
     }
 
-    service.gcm.send(SAMPLE_TAG, GCM_MESSAGE, (error, res) => {
-        if (error) {
-            console.log(error);
-            return;
-        }
+    console.log('Registration success');
+    console.log(JSON.stringify(response));
 
-        console.log('Message sent');
+    service.apns.send(SAMPLE_TAG, APNS_MESSAGE, (error, res) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      console.log('Message sent');
+      console.log(JSON.stringify(res));
     });
+  });
+}
 
-});
+if (GCM_REGISTRATION_ID) {
+  service.gcm.createNativeRegistration(GCM_REGISTRATION_ID, SAMPLE_TAG, (err, response) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
+    console.log('Registration success');
+    console.log(JSON.stringify(response));
 
+    service.gcm.send(SAMPLE_TAG, GCM_MESSAGE, (error, res) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+
+      console.log('Message sent');
+      console.log(JSON.stringify(res));
+    });
+  });
+}
 
